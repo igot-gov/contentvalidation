f5aaaeeee9698460de053cea38e8dccc65bcd53a
karthik-tarento <karthikeyan.rajendran@tarento.com>
2020-10-21 12:31:07 +0530

    Added branch to check-in template code

---
+++ src/main/java/com/eagle/contentvalidation/ContentValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eagle.contentvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentValidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentValidationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eagle/contentvalidation/ContentValidationServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eagle.contentvalidation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContentValidationServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
