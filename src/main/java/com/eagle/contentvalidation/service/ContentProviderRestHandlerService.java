4789ff0cf2073c862094e0c55f78cff36b7c4a42
karthik-tarento <karthikeyan.rajendran@tarento.com>
2020-11-23 13:10:31 +0530

    Minor changes

--- src/main/java/com/eagle/contentvalidation/service/ContentProviderRestHandlerService.java
+++ src/main/java/com/eagle/contentvalidation/service/ContentProviderRestHandlerService.java
@@ -1,10 +1,7 @@
 package com.eagle.contentvalidation.service;
 
-import org.springframework.stereotype.Service;
-
 import com.eagle.contentvalidation.model.ContentPdfValidation;
 
-@Service
 public interface ContentProviderRestHandlerService {
 
 	public void handleStartContentValidationRequest(ContentPdfValidation contentPdfValidation);
--- src/main/java/com/eagle/contentvalidation/service/impl/ContentProviderRestHandlerServiceImpl.java
+++ src/main/java/com/eagle/contentvalidation/service/impl/ContentProviderRestHandlerServiceImpl.java
@@ -1,12 +1,14 @@
 package com.eagle.contentvalidation.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.eagle.contentvalidation.config.Configuration;
 import com.eagle.contentvalidation.model.ContentPdfValidation;
 import com.eagle.contentvalidation.producer.StartContentValidationProducer;
 import com.eagle.contentvalidation.service.ContentProviderRestHandlerService;
 
+@Service
 public class ContentProviderRestHandlerServiceImpl implements ContentProviderRestHandlerService {
 
 	@Autowired
