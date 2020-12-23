16375f01516618bbbc6d9cf3583b3a1711168b23
amit-kumaryadav <amtydv630@gmail.com>
2020-12-23 15:39:39 +0530

    Removed un-used import

--- src/main/java/com/eagle/contentvalidation/controller/ContentValidationController.java
+++ src/main/java/com/eagle/contentvalidation/controller/ContentValidationController.java
@@ -1,29 +1,17 @@
 package com.eagle.contentvalidation.controller;
 
-import java.io.IOException;
-import java.util.List;
-
+import com.eagle.contentvalidation.model.ContentPdfValidation;
+import com.eagle.contentvalidation.model.ContentPdfValidationResponse;
+import com.eagle.contentvalidation.repo.ContentValidationRepoServiceImpl;
+import com.eagle.contentvalidation.repo.model.PdfDocValidationResponse;
+import com.eagle.contentvalidation.service.ContentProviderRestHandlerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.eagle.contentvalidation.model.ContentPdfValidation;
-import com.eagle.contentvalidation.model.ContentPdfValidationResponse;
-import com.eagle.contentvalidation.model.ProfanityResponseWrapper;
-import com.eagle.contentvalidation.repo.ContentValidationRepoServiceImpl;
-import com.eagle.contentvalidation.repo.model.PdfDocValidationResponse;
-import com.eagle.contentvalidation.service.ContentProviderRestHandlerService;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1")
@@ -47,14 +35,7 @@ public class ContentValidationController {
 			@RequestBody ContentPdfValidation contentPdfValidation) {
 		return new ResponseEntity<>(contentValidationRepoService.getContentValidationResponseForFile(contentPdfValidation.getContentId(), contentPdfValidation.getFileName()), HttpStatus.OK);
 	}
-
-	@PostMapping(value = "/startLocalPdfProfanity", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<ContentPdfValidationResponse> startLocalContentPdfProfanity(
-			@RequestBody ContentPdfValidation contentPdfValidation){
-		restHandler.handleStartContentValidationRequest(contentPdfValidation);
-		return new ResponseEntity<>(HttpStatus.ACCEPTED);
-	}
-
+	
 	@PostMapping(value = "/getLocalPdfProfanity", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<PdfDocValidationResponse> getLocalContentPdfProfanity(
 			@RequestBody ContentPdfValidation contentPdfValidation) {
