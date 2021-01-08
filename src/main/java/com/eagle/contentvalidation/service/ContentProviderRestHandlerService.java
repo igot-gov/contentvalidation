610200b9a1b416f6da27d73a0b6a8649a94da29b
@somvit.bhowmik <somvit.bhowmik@tarento.com>
2021-01-08 18:09:41 +0530

    Changed package name and removed copyright
    Changed package name and removed copyright

--- src/main/java/com/eagle/contentvalidation/service/ContentProviderRestHandlerService.java
+++
@@ -1,8 +0,0 @@
-package com.eagle.contentvalidation.service;
-
-import com.eagle.contentvalidation.model.ContentPdfValidation;
-
-public interface ContentProviderRestHandlerService {
-
-	public void handleStartContentValidationRequest(ContentPdfValidation contentPdfValidation);
-}
